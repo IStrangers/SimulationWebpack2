4bccd0a4f41f1d9926395cc208a48ac208423df9
qqq000qqq@aliyun.com <xqq1258460261>
2022-09-01 16:29:05 +0800

    20220901

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path")
 module.exports = {
   mode: "development",
   devtool: false,
+  context: "",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname,"dist"),
